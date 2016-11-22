202370378e07df26a8862e937e58b3f2b87896f6
JoseLuisPucChan <josechan211@gmail.com>
2016-11-21 21:22:23 -0600

    Con

--- Proyecto integrador/Web/MCTuristic_Centro_Historico/WSMCTuristic_CentroHistorico/DAO/Conexion.cs
+++ Proyecto integrador/Web/MCTuristic_Centro_Historico/WSMCTuristic_CentroHistorico/DAO/Conexion.cs
@@ -13,7 +13,7 @@ namespace WSMCTuristic_CentroHistorico.DAO
 
         public SqlConnection Establecer()
         {
-            sql = "Data source = ANDRE-PC\\SQLSERVER; Initial Catalog=MCTuristic; Integrated Security=True";
+            sql = "Data source = 192.168.100.4; Initial Catalog=MCTuristic; password=1234; Integrated Security=True";
             //sql = "Data source =192.168.100.4; Initial Catalog=MCTuristic;User id=user;password=1234";
             conn = new SqlConnection(sql);
             return conn;
