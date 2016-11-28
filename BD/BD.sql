e4b742dcef00efcd81c7fb9f056948323008e2e1
AtomicLotus <ghs_andre@hotmail.com>
2016-11-28 04:41:27 -0600

    :v:

--- BD/BD.sql
+++ BD/BD.sql
@@ -1,3 +1,50 @@
+--------------------Función para encriptar contraseña
+CREATE FUNCTION fnColocaClave 
+(
+    @clave VARCHAR(25)
+)
+RETURNS VarBinary(8000)
+AS
+BEGIN
+    DECLARE @pass AS VarBinary(8000)
+    SET @pass = ENCRYPTBYPASSPHRASE('dbCurso09',@clave)
+    RETURN @pass
+
+END
+go
+--------------------Función para desencriptar contraseña
+CREATE FUNCTION fnLeeClave 
+(
+    @clave VARBINARY(8000)
+)
+RETURNS VARCHAR(25)
+AS
+BEGIN
+    DECLARE @pass AS VARCHAR(25)
+    SET @pass = DECRYPTBYPASSPHRASE('dbCurso09',@clave)
+    RETURN @pass
+
+END
+GO
+
+------------------para ver encriptada la contraseña
+SELECT IdUsuario, nombre, apellidos, email, dbo.fnColocaClave(contrasena) as [Contraseña]
+FROM Usuario
+GO
+
+------------------para desifrar contraseña
+SELECT IdUsuario, nombre, apellidos, email, dbo.fnLeeClave(contrasena) as [Contraseña]
+FROM Usuario
+GO
+-----------------------enviarcorreos
+EXEC msdb.dbo.sp_send_dbmail
+    @profile_name = 'José Luis',
+    @recipients = 'josecha211@gmail.com',
+    @body = 'La prueba funciono satisfactoriamente',
+    @subject = 'Importante' ;
+GO
+
+
 -----------Creamos el procedimiento almacenado para poder hacer el backup con la fecha y hora.
 ------------Creare un ejemplo con la base de datos de mi proyecto MCTurisctic
 
@@ -10,32 +57,31 @@ MEDIANAME = 'D_SQLServerBackups', NAME = 'FULL Backup';
 Go
 ---------------------Declarada ahora generemos la automatización
 
-CREATE LOGIN adminMC WITH PASSWORD = '123'
-CREATE USER adminMC FOR LOGIN adminMC
+---- creación de nuevos inicios de sesión y usuarios
+CREATE LOGIN AdministradorNuevo WITH PASSWORD = '123'
+CREATE USER AdministradorNuevo FOR LOGIN AdministradorNuevo
 GO
-CREATE LOGIN usuarioMC WITH PASSWORD = '123'
-CREATE USER usuarioMC FOR LOGIN usuarioMC
+CREATE LOGIN UsuarioNuevo WITH PASSWORD = '123'
+CREATE USER UsuarioNuevo FOR LOGIN UsuarioNuevo
 GO
-CREATE SCHEMA Escuela1 AUTHORIZATION adminMC
-CREATE TABLE Carrera (
-idCarrera INT IDENTITY(1,1)
+
+---- Conceder permisos de administrador a un usuario de windows
+sp_addsrvrolemember 'AdministradorNuevo', 'sysadmin'
+go
+
+---- Creación de una base de datos con autorizaciones para los usuarios: UsuarioNuevo y AdministradorNuevo
+CREATE DATABASE Jardin
+go
+use Jardin
+go
+CREATE SCHEMA Verde AUTHORIZATION AdministradorNuevo
+go
+CREATE TABLE Plantas (
+idPlanta INT IDENTITY(1,1)
 ,Descripcion NVARCHAR(100)
 ,Estatus CHAR(1)
-,CONSTRAINT [PK_Carrera] PRIMARY KEY (idCarrera)
+,CONSTRAINT [PK_Planta] PRIMARY KEY (idPlanta)
 )
-CREATE TABLE Materia (
-idMateria INT IDENTITY(1,1)
-,Descripcion NVARCHAR(100)
-,idCarrera INT
-,Estatus CHAR(1)
-,CONSTRAINT [PK_Materia] PRIMARY KEY (idMateria)
-,CONSTRAINT [FK_Materia_Carrera] FOREIGN KEY (idCarrera)
-REFERENCES [Carrera] (idCarrera)
-)
-GRANT SELECT, INSERT ON Escuela1.Carrera TO usuarioMC
+GRANT SELECT, INSERT ON Jardin.Plantas TO UsuarioNuevo
 GO
-CREATE TABLE Escuela1.Maestro(
-idMaestro int identity(1,1)
-,Nombre nvarchar(100)
-,CONSTRAINT [PK_Maestro] PRIMARY KEY (idMaestro)
-)
+
