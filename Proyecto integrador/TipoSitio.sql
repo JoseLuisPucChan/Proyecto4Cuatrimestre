3d38d20c1ada266586e0313c98c86b6dcb715a3d
Straiker10 <smurs_14@hotmail.com>
2016-11-29 21:26:18 -0600

    scriptTipoServicio

---
+++ Proyecto integrador/TipoSitio.sql
@@ -0,0 +1,11 @@
+select * from TipoSitio
+
+INSERT INTO TipoSitio(NombreSitio,DescripcionSitio) VALUES ('Museo','Lugar donde se encuentran obras prehistoricas')
+INSERT INTO TipoSitio(NombreSitio,DescripcionSitio) VALUES ('Parque','Lugar de diversion publico')
+INSERT INTO TipoSitio(NombreSitio,DescripcionSitio) VALUES ('Esquina','Calle conmemorativa de Yucatán')
+INSERT INTO TipoSitio(NombreSitio,DescripcionSitio) VALUES ('Monumento','Simbolo de alguien famoso')
+INSERT INTO TipoSitio(NombreSitio,DescripcionSitio) VALUES ('Plaza','Lugares para eventos de Yucatán')
+INSERT INTO TipoSitio(NombreSitio,DescripcionSitio) VALUES ('Casona','Casas antiguas connmemorativas')
+INSERT INTO TipoSitio(NombreSitio,DescripcionSitio) VALUES ('Teatro','Lugar donde se realizan obras')
+INSERT INTO TipoSitio(NombreSitio,DescripcionSitio) VALUES ('Barrio','Asentamientos historicos con iglesia')
+INSERT INTO TipoSitio(NombreSitio,DescripcionSitio) VALUES ('Iglesia','Lugar Catolico')
