df0622c657dc1fa9dc2e1d7fbdf38635943568f5
JoseLuisPucChan <josechan211@gmail.com>
2016-11-30 21:24:28 -0600

    Cadena Conexion

--- Proyecto integrador/Web/Proyecto 1.2 proyeco Reparado/MCTuristic_Centro_Historico/WSMCTuristic_CentroHistorico/DAO/Conexion.cs
+++ Proyecto integrador/Web/Proyecto 1.2 proyeco Reparado/MCTuristic_Centro_Historico/WSMCTuristic_CentroHistorico/DAO/Conexion.cs
@@ -15,8 +15,8 @@ namespace WSMCTuristic_CentroHistorico.DAO
         public SqlConnection Establecer()
         {
             //sql = "Data source = ANDRE-PC\\SQLSERVER; Initial Catalog=MCTuristic; Integrated Security=True";
-            //sql = "Data source =25.65.245.232; Initial Catalog=MCTuristic;User id=user;password=1234";
-            sql = "Data source = LAPTOP-U1DIF2PF\\SQLSERVER; Initial Catalog=MCTuristic; Integrated Security=True";
+            sql = "Data source =25.65.245.232; Initial Catalog=MCTuristic;User id=user;password=1234";
+            //sql = "Data source = LAPTOP-U1DIF2PF\\SQLSERVER; Initial Catalog=MCTuristic; Integrated Security=True";
             
             conn = new SqlConnection(sql);
             return conn;
